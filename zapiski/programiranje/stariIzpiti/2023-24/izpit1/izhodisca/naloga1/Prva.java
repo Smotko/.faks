2d54b54e99ac9f5ea3bffe08aad510b40b77513f
Smotko <anze.stanonik.104a@gmail.com>
2024-12-27 18:40:37 +0100

    A

--- zapiski/programiranje/stariIzpiti/2023-24/izpit1/izhodisca/naloga1/Prva.java
+++ zapiski/programiranje/stariIzpiti/2023-24/izpit1/izhodisca/naloga1/Prva.java
@@ -15,7 +15,23 @@ import java.util.*;
 public class Prva {
 
     public static void main(String[] args) {
-        // dopolnite ...
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ploscine;
+        boolean b;
+        
+        for(int i = 0; i < n; i++){
+            ploscine = sc.nextInt();
+            b = false;
+            for(int j = 1; j < Math.sqrt(ploscine) * 2; j ++){
+                for(int k = 1; k < Math.sqrt(ploscine) * 2; k ++){
+                    if(j*k == ploscine && j > k/2 && k > j/2)
+                        b = true;
+                }
+            }
+            System.out.println( b ? "DA" : "NE");
+        }
     }
 
     // pomo"zne metode, notranji razredi (po potrebi) ...
--- zapiski/programiranje/stariIzpiti/2023-24/izpit1/izhodisca/naloga2/Druga.java
+++ zapiski/programiranje/stariIzpiti/2023-24/izpit1/izhodisca/naloga2/Druga.java
@@ -17,6 +17,40 @@ public class Druga {
 
     public static void main(String[] args) {
         // dopolnite ...
+        Scanner sc = new Scanner(System.in);
+        int nacin = sc.nextInt();
+        int stDraziteljev = sc.nextInt();
+        int stIzdelkov = sc.nextInt();
+        int stPonudb = sc.nextInt();
+        int najPonudba[] = new int[stIzdelkov];
+        int ponudbNaDrazitelja[] = new int[stDraziteljev];
+        int draziteljiSpomin[] = new int[stIzdelkov];
+
+        for(int i = 0; i < draziteljiSpomin.length; i++){
+            draziteljiSpomin[i] = -1;
+        }
+
+        for(int i = 0; i < stPonudb; i++){
+            int drazitelj = sc.nextInt();
+            int izdelek = sc.nextInt();
+            int ponudba = sc.nextInt();
+            
+            
+
+            if(najPonudba[izdelek] < ponudba){
+                najPonudba[izdelek] = ponudba;
+                ponudbNaDrazitelja[drazitelj] ++;
+
+                if(draziteljiSpomin[izdelek] > -1){
+                    ponudbNaDrazitelja[draziteljiSpomin[izdelek]] --;
+                }
+
+                draziteljiSpomin[izdelek] = drazitelj;
+            }
+        }
+        System.out.println((nacin == 1) ? Arrays.toString(najPonudba) : Arrays.toString(ponudbNaDrazitelja));
+
+
     }
 
     // pomo"zne metode, notranji razredi (po potrebi) ...
